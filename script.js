411a0c09f3a36e02a23c0d94c7dbcfff6fd3f608
achrafmb <1999010104087@ofppt-edu.ma>
2023-09-27 17:08:56 +0200

    add starbucks website

---
+++ script.js
@@ -0,0 +1,7 @@
+function imgSlider(image){
+    document.getElementById('curr-image').src = image;
+}
+
+function changeColor(color){
+    document.getElementById('circle').style.backgroundColor = color;
+}
